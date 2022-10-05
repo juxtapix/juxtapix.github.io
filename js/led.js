aae6fd1889c8f81649aad7014320442ce34908b6
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2022-10-05 19:49:21 -0400

    Add files via upload

---
+++ js/led.js
@@ -0,0 +1,74 @@
+(function() {
+  'use strict';
+
+  document.addEventListener('DOMContentLoaded', event => {
+    let connectButton = document.querySelector("#connect");
+    let statusDisplay = document.querySelector('#status');
+    let lightButton = document.querySelector('#light');
+    let toggle = 0;
+    let port;
+
+    function connect() {
+      port.connect().then(() => {
+        statusDisplay.textContent = '';
+        connectButton.textContent = 'Disconnect';
+
+        port.onReceive = data => {
+          let textDecoder = new TextDecoder();
+          console.log(textDecoder.decode(data));
+        }
+        port.onReceiveError = error => {
+          console.error(error);
+        };
+      }, error => {
+        statusDisplay.textContent = error;
+      });
+    }
+
+    function onClick(){
+      if (!port) {
+        return;
+      }
+
+      let view = new Uint8Array(1);
+      if (toggle == 0){
+        view[0] = parseInt(72);
+        console.log(view);
+        toggle = 1;
+      } else {
+        view[0] = parseInt(76);
+        console.log(view);
+        toggle = 0;
+      }
+      port.send(view);
+
+    };
+
+    lightButton.addEventListener('click', onClick);
+    connectButton.addEventListener('click', function() {
+      if (port) {
+        port.disconnect();
+        connectButton.textContent = 'Connect';
+        statusDisplay.textContent = '';
+        port = null;
+      } else {
+        serial.requestPort().then(selectedPort => {
+          port = selectedPort;
+          connect();
+        }).catch(error => {
+          statusDisplay.textContent = error;
+        });
+      }
+    });
+
+    serial.getPorts().then(ports => {
+      if (ports.length == 0) {
+        statusDisplay.textContent = 'No device found.';
+      } else {
+        statusDisplay.textContent = 'Connecting...';
+        port = ports[0];
+        connect();
+      }
+    });
+  });
+})();
