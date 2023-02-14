c9ee34832d1fbdf90815f68101b3793782fda2a1
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2023-02-13 19:12:02 -0500

    Delete examples directory

--- examples/light_sensor/js/slider.js
+++
@@ -1,6 +0,0 @@
-function updateSlider(sliderValue) {
-    var output = document.getElementById("myValue");
-    output.innerHTML = sliderValue;
-
-    document.getElementById("myCircle").style.backgroundColor = "hsl(0, 0%, " + sliderValue + "%)";
-}
